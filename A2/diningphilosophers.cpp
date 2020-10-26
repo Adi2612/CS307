7c6cade6e9f6d79f054ead42501e4cea0203bc5d
anvay09 <shah.anvay@gmail.com>
2020-10-26 18:00:52 +0530

    Dining Philosophers complete version, Run 1

--- A2/diningphilosophers.cpp
+++ A2/diningphilosophers.cpp
@@ -4,10 +4,15 @@
 #include <semaphore.h>
 #include <chrono>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
+
 #define N 5 // 5 philosophers
 #define RUNTIME 1800 //runtime = 30 minutes = 1800 seconds
+
+void* thread_Subroutine(void* p_ID);
+void change_State(int i, string state);
 
 pthread_t thread_ID[N]; //used in pthread_create and p_thread_join
 int philosopher_ID[N] = {0,1,2,3,4}; // assigning an integer ID to each philosopher
@@ -16,6 +21,8 @@ string philosopher_state[N] = {"Thinking", "Thinking", "Thinking", "Thinking", "
 sem_t state_lock; //semaphore used to lock philosopher_state before changing it
 sem_t forks[N]; //semaphore array used for forks
 string output_file;
+
+int eat_count[N] = {0,0,0,0,0}; //counts number of times each philosopher eats
 
 void init_Semaphores() // initialize all semaphores to 1
 {
@@ -47,12 +54,25 @@ void join_Threads()
 
 void print_State() //call function from inside thread only if state is locked, prints state of all philosophers to output file
 {
-    fstream out;
+    ofstream out;
     out.open(output_file, ios::app);
 
     for (int i = 0; i < N; i++)
     {
         out<<"Philosopher "<<i<<" state : "<<philosopher_state[i]<<endl;
+    }
+    
+    out.close();
+}
+
+void print_Count()
+{
+    ofstream out;
+    out.open(output_file, ios::app);
+
+    for (int i = 0; i < N; i++)
+    {
+        out<<"Philosopher "<<i<<" count : "<<eat_count[i]<<endl;
     }
     
     out.close();
@@ -68,20 +88,41 @@ void change_State(int i, string state)
 
 void dine(int p_ID)
 {
+    if (p_ID != N-1)
+    {
+        sem_wait(&forks[p_ID]);
 
+        change_State(p_ID, "Fork picked up");
+
+        sem_wait(&forks[(p_ID + 1)%N]);
+    }
+    else if (p_ID == N-1)
+    {
+        sem_wait(&forks[(p_ID + 1)%N]);
+
+        change_State(p_ID, "Fork picked up");
+
+        sem_wait(&forks[p_ID]);
+    }
+    
+    change_State(p_ID, "Eating");
+    sleep(1); //eat for 1 seconds
+    eat_count[p_ID]++; //increment count
+    sem_post(&forks[p_ID]);
+    sem_post(&forks[(p_ID + 1)%N]);
 }
 
 void* thread_Subroutine(void* p_ID) //supplied to pthread_create
 {
-    chrono::steady_clock::timepoint start_time = chrono::steady_clock::now();
+    auto start_time = chrono::steady_clock::now();
 
-    while (std::chrono::steady_clock::now() - start_time < std::chrono::seconds(RUNTIME)) //run for RUNTIME
+    while (chrono::steady_clock::now() - start_time < chrono::seconds(RUNTIME)) //run for RUNTIME
     {
         dine(*(int *)p_ID); //eat
         change_State(*(int *)p_ID, "Thinking"); //think
+        sleep(rand()%5 + 1); //think for random time
     }
 }
-
 
 int main(int argc, char const *argv[]) 
 {
@@ -94,12 +135,14 @@ int main(int argc, char const *argv[])
         cout<<"Please provide output file to write to"<<endl;
         return 0;
     }
-    
+
+    print_State();
     
     init_Semaphores();
-
     create_Threads();
     join_Threads();
+
+    print_Count();
 
     return 0;
 }
